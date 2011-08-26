ccc09375c942891e07c9dfaa2e0087fc458bd46a
Li Chaoqun <lee.chaoqun@gmail.com>
2011-08-26 21:59:53 +0800

    changed output report migration

--- db/migrate/20110825151437_change_user_output_report_menu_under_agent.rb
+++ db/migrate/20110825151437_change_user_output_report_menu_under_agent.rb
@@ -2,8 +2,10 @@ class ChangeUserOutputReportMenuUnderAgent < ActiveRecord::Migration
   def self.up
     menu = Menu.where(:url => "reports_agent_user_output_path").first
     menu_group = MenuGroup.where(:category => "agent", :seq => 6).first
-    menu.menu_group = menu_group
-    menu.save
+    if menu
+      menu.menu_group = menu_group
+      menu.save
+    end
   end
 
   def self.down
