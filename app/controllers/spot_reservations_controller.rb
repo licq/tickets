894785ba248de55bd13785c855f9846832609550
Li Chaoqun <lee.chaoqun@gmail.com>
2011-08-29 21:50:59 +0800

    fixed a bug

--- app/controllers/spot_reservations_controller.rb
+++ app/controllers/spot_reservations_controller.rb
@@ -25,37 +25,50 @@ class SpotReservationsController < ApplicationController
 
   def update
     @reservation = @spot.reservations.find(params[:id])
-    if params[:adult_true_ticket_number].present? && params[:child_true_ticket_number].present?
-      if @reservation.adult_true_ticket_number < params[:adult_true_ticket_number] || @reservation.child_true_ticket_number < params[:child_true_ticket_number]
-        @reservation.errors.add("成人数和儿童数不能大于以前订单上的相应数值")
-        render :edit
-        return
-      end
-    end
-    if @reservation.update_attributes(params[:team_reservation] || params[:individual_reservation])
-      if @reservation.adult_true_ticket_number.blank? || @reservation.adult_true_ticket_number == 0
-        @reservation.errors.add(:adult_true_ticket_number, "实到成人数必须大于0")
-        render :edit
-        return
-      end
-      @reservation.set_true_total_price
-      @reservation.date = Date.today
-      @reservation.status = :checkedin
-      @reservation.paid = true if @reservation.payment_method == 'poa'
-      if (@reservation.type == 'TeamReservation' && @reservation.payment_method == 'poa')
-        @reservation.settled = true
+    reservation_params = params[:individual_reservation] || params[:team_reservation]
+    if reservation_params["adult_true_ticket_number"].present? || reservation_params["child_true_ticket_number"].present?
+      validation_failed = false
+      if @reservation.adult_ticket_number < reservation_params[:adult_true_ticket_number].to_i || (@reservation.adult_true_ticket_number.present? && @reservation.adult_true_ticket_number < reservation_params[:adult_true_ticket_number].to_i)
+        @reservation.errors.add(:adult_true_ticket_number, "成人数不能大于以前订单上的相应数值")
+        validation_failed = true
       end
 
-      if (@reservation.payment_method == "net" && @reservation.book_purchase_price == @reservation.total_purchase_price)
-        @reservation.settled = true
-      else
-        @reservation.settled = false
+      if  @reservation.child_ticket_number < reservation_params[:child_true_ticket_number].to_i || (@reservation.child_true_ticket_number.present? && @reservation.child_true_ticket_number < reservation_params[:child_true_ticket_number].to_i)
+        @reservation.errors.add(:child_true_ticket_number, "儿童数不能大于以前订单上的相应数值")
+        validation_failed = true
       end
-      @reservation.save!
-      redirect_to today_spot_reservations_url, :notice => "已入园成功."
-    else
-      render :action => 'edit'
+
+      if validation_failed
+        render :edit
+        return
+      end
     end
+
+
+  if @reservation.update_attributes(params[:team_reservation] || params[:individual_reservation])
+    if @reservation.adult_true_ticket_number.blank? || @reservation.adult_true_ticket_number == 0
+      @reservation.errors.add(:adult_true_ticket_number, "实到成人数必须大于0")
+      render :edit
+      return
+    end
+    @reservation.set_true_total_price
+    @reservation.date = Date.today
+    @reservation.status = :checkedin
+    @reservation.paid = true if @reservation.payment_method == 'poa'
+    if (@reservation.type == 'TeamReservation' && @reservation.payment_method == 'poa')
+      @reservation.settled = true
+    end
+
+    if (@reservation.payment_method == "net" && @reservation.book_purchase_price == @reservation.total_purchase_price)
+      @reservation.settled = true
+    else
+      @reservation.settled = false
+    end
+    @reservation.save!
+    redirect_to today_spot_reservations_url, :notice => "已入园成功."
+  else
+    render :action => 'edit'
   end
+end
 
 end
